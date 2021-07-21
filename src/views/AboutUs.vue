c474c6eb7e8ec65bc6552a0afc5360d645712114
Veyseng Choeng <choeng.veyseng1@gmail.com>
2021-07-21 19:40:04 +0700

    [IMP] Add on home page navbar and the format

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -3,3 +3,15 @@
     <h1>Coming soon</h1>
   </div>
 </template>
+<script>
+export default {
+  setup() {
+    
+  },
+}
+</script>
+<style scoped>
+  .about{
+    margin-top: 10rem;
+  }
+</style>
--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -3,3 +3,8 @@
     <h1>Coming soon</h1>
   </div>
 </template>
+<style scoped>
+  .contact{
+    margin-top: 10rem;
+  }
+</style>
